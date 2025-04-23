acd4c00d620bd75416a7839578d87c904278bb39
MuhibullohHakimjonov <mukhabillo@gmail.com>
2025-04-23 13:35:15 +0500

    Update db_middleware.py

--- db_middleware.py
+++ db_middleware.py
@@ -12,9 +12,7 @@ class DbSessionMiddleware(BaseMiddleware):
         self.session_factory = session_factory
 
     async def __call__(self, handler, event: Update, data: dict):
-        logging.info(f"Middleware processing event: {event}")
         async with self.session_factory() as session:
             data["session"] = session
             result = await handler(event, data)
-            logging.info(f"Middleware finished processing event: {event}")
             return result
